bfd8924121d9e4b57773a6fccd42d5383d04c1ce
luckylooky2 <luckylooky2@naver.com>
2024-10-30 00:47:15 +0900

    Feat: [react-deep-dive] hydrate

--- 모던 리액트 Deep Dive/4장. 서버 사이드 렌더링/hydrate/hydrate.js
+++ 모던 리액트 Deep Dive/4장. 서버 사이드 렌더링/hydrate/hydrate.js
@@ -1,28 +1,29 @@
 const { useState, useEffect } = React;
 
-// function App() {
-//   return (
-//     <div id="root" data-reactroot="">
-//       <div>hello</div>
-//       <ul>
-//         <li>apple</li>
-//         <li>banana</li>
-//         <li>peach</li>
-//       </ul>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div id="root" data-reactroot="">
+      <div>hello</div>
+      <ul>
+        <li>apple</li>
+        <li>banana</li>
+        <li>peach</li>
+        <button onClick={() => console.log("+")}>+</button>
+      </ul>
+    </div>
+  );
+}
 
 // 불일치가 발생하면, hydrate에 전달된 컴포넌트를 기준으로 웹 페이지를 그린다.
 // - 사실상, 두 번의 렌더링이 발생하는 것이기 때문에 반드시 고쳐야 하는 문제이다.
-function App() {
-  return (
-    <>
-      <span>hello</span>
-      <div suppressHydrationWarning>{new Date().getTime()}</div>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <span>hello</span>
+//       <div suppressHydrationWarning>{new Date().getTime()}</div>
+//     </>
+//   );
+// }
 
 // 불가피하게 결과물이 다를 수 있을 수도 있다.
 // - 컴포넌트 내부에 초 단위 시간이 렌더링 되는 경우, suppressHydrationWarning를 추가해 경고를 끌 수 있다.
